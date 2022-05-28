eaa08f866e7881cdbbc5adc9690182aa10cd3435
Erlend Eliassen <gloatingcord26@github.com>
2022-05-28 16:25:42 +0200

    Chore: Better namespace names

--- src/StringFuncs.php
+++ src/StringFuncs.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Main;
+namespace BasicStringFunctions\Main;
 
 class StringFuncs implements StringInterface
 {
--- src/StringInterface.php
+++ src/StringInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Main;
+namespace BasicStringFunctions\Main;
 
 interface StringInterface
 {
